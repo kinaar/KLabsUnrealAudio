11d95bd6885b6b1b15b88aad412dc5695529cbc7
Guerric <guerric.chab@gmail.com>
2020-07-09 20:19:40 +0200

    Unreal Added

---
+++ Unreal/KLabsUnrealAudio/Source/KLabsUnrealAudio/KLabsUnrealAudioGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KLabsUnrealAudioGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KLABSUNREALAUDIO_API AKLabsUnrealAudioGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
